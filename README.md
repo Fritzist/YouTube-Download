f4f5fe6e0499431aa2e2216b001c02ed9b9b2e06
Fritzist <danielhartl856@gmail.com>
2022-06-27 13:35:42 +0200

    lets go!

---
+++ README.md
@@ -0,0 +1,9 @@
+#YouTube Download
+<hr>
+
+    Language: Python
+    Package: pytube
+
+##YouTube Download is a simple Python script that downloads videos from YouTube.
+
+<hr>
---
+++ main.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+
+link = input("Enter the link from the video you want to download: ")
+youtube = YouTube(link)
+
+path = input("Enter the path where you want to save the video: ")
+videoname = input("Enter the name of the video: ")
+
+video = youtube.streams.get_highest_resolution()
+video.download(path, filename=videoname + ".mp4")
+
+print(videoname.upper() + " was downloaded successfully! Check your path ( " + path +" ) for the video.")
